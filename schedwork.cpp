1044975e3c132cb258f16700ae4da6a49e4dc6bc
Cindy Zhou <cyzhou@usc.edu>
2025-04-19 22:10:40 +0000

    Initial commit for hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shiftCount,
+    unsigned int day, 
+    unsigned int workers
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +45,67 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    // avail = rows x col (days x workers)
+    size_t numDays = avail.size();
+    size_t numWorkers = avail[0].size();
+
+    // sched rows x col (days x dailyNeed)
+    for (unsigned int i = 0; i < numDays; i++)
+    {
+        vector<Worker_T> days;
+        sched.push_back(days);
+    }
+    vector<size_t> shiftCount(numWorkers, 0);
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftCount, 0, 0);
+
+}
 
 
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shiftCount,
+    unsigned int day, 
+    unsigned int workers
+) 
+{
+    // BASE CASE: schedule completed
+    if (day == avail.size())
+    {
+        return true;
+    }
+    // ITERATE: through all options
+    for(int i = 0; i < (int) avail[0].size(); i++)
+    {
+        /* IS VALID CONDITIONS:
+         * 1. avalibility         
+         * 2. worker does not work more than maxShifts
+         * 3. there is at least shiftCount workers working per day */
 
+        // CONDITIONS 1 and 2
+        if (avail[day][i] && shiftCount[i] < maxShifts) 
+        {
+            sched[day].push_back(i);
+            shiftCount[i]++;
 
+            // CONDITION 3: only move onto the next day if current day has enough workers 
+            if (workers + 1 == dailyNeed) 
+            {
+                return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftCount, day + 1, 0);
+            }
+            // All options to get to the end of the day
+            if (scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftCount, day, workers + 1))
+            {
+                return true;
+            }
+
+            // BACKTRACK
+            sched[day].pop_back();
+            shiftCount[i]--;
+        }
+    }
+    return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,13 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(
+    std::string& in,
+    std::map<char, int>& float_map,
+    const std::set<std::string>& dict, 
+    unsigned int index,
+    std::set<std::string>& solution
+);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +27,88 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    std::set<std::string> solution;     // to return
+    std::string in_copy = in;           // copy to be modified
+    std::map<char, int> float_map;      // hold all in letters
 
+    for (char c : floating)
+    {
+        float_map[c]++;
+    }
+
+    wordleHelper(in_copy, float_map, dict, 0, solution);
+    return solution;
 }
 
+
 // Define any helper functions here
+void wordleHelper(
+    std::string& in,
+    std::map<char, int>& float_map,
+    const std::set<std::string>& dict, 
+    unsigned index,
+    std::set<std::string>& solution
+)
+{
+    // BASE CASE
+    if (index == in.size())
+    {
+        // all floating letters were used
+        if (!float_map.empty())
+        {
+            return;
+        }
+        // word exists in dictionary
+        if (dict.count(in))
+        {
+            solution.insert(in);
+        }
+        return;
+    }
+
+    // SKIP: if letter is already filled in
+    if (in[index] != '-')
+    {
+        wordleHelper(in, float_map, dict, index + 1, solution);
+    }
+    // RECURSE: 
+    else
+    {
+        // LIMITATIONS: '-' must be more then number of floating letters left
+        int dashes = 0;
+        int floatingChars = 0;
+        for (char c : in)
+        {
+            if (c == '-')
+            {
+                dashes++;
+            }
+        }
+        std::map<char, int>::iterator it;
+        for (it = float_map.begin(); it != float_map.end(); ++it)
+        {
+            floatingChars += it->second;
+        }
+        if (floatingChars > dashes)
+        {
+            return;
+        }
+        // RECURSE: all combinations
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            in[index] = c;
+            std::map<char, int> updated_float_map = float_map;
+            // If we use a floating letter, remove from map
+            if (updated_float_map.count(c))
+            {
+                updated_float_map[c]--;
+                if (updated_float_map[c] == 0)
+                {
+                    updated_float_map.erase(c);
+                }
+            }
+            wordleHelper(in, updated_float_map, dict, index + 1, solution);
+        }
+        in[index] = '-'; // backtrack
+    }
+}
